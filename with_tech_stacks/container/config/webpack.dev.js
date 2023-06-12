33aef51a2ba97c18d95a352400b96739e07b753c
ntp897 <phuocnt@cnv.vn>
2023-06-12 22:23:08 +0700

    create container deploy script

--- with_tech_stacks/container/config/webpack.dev.js
+++ with_tech_stacks/container/config/webpack.dev.js
@@ -1,9 +1,8 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
-const packageJson = require("../package.json");
+// const packageJson = require("../package.json");
 
 const devConfig = {
   mode: "development",
@@ -14,9 +13,6 @@ const devConfig = {
     },
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
