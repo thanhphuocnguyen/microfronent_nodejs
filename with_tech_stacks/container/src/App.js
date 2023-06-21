6ca607aa577bbe6b30771b1e67645e3b4d1adee5
ntp897 <phuocnt@cnv.vn>
2023-06-21 18:14:12 +0700

    change to test workflow

--- with_tech_stacks/container/src/App.js
+++ with_tech_stacks/container/src/App.js
@@ -3,6 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
+      <h2>Microfrontend test invalidation</h2>
       <MarketingApp />
     </div>
   );
