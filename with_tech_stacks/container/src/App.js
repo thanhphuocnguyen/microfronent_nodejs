13617870ce1547c1ad858f4953d56810de30a28e
ntp897 <phuocnt@cnv.vn>
2023-06-24 12:39:02 +0700

    added header

--- with_tech_stacks/container/src/App.js
+++ with_tech_stacks/container/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
 export default () => {
   return (
-    <div>
+    <BrowserRouter>
+      <Header />
       <MarketingApp />
-    </div>
+    </BrowserRouter>
   );
 };
